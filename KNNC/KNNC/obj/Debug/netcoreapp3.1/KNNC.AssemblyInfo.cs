216eaca68089770f2710e4f873974725910e6f7e
Çağatay <cagatay.kapuagasi@gmail.com>
2020-06-07 00:44:54 +0300

    Convert .netcoreapp

--- KNNC/KNNC/obj/Debug/netcoreapp3.1/KNNC.AssemblyInfo.cs
+++ KNNC/KNNC/obj/Debug/netcoreapp3.1/KNNC.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("KNN")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("KNNC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("KNN")]
-[assembly: System.Reflection.AssemblyTitleAttribute("KNN")]
+[assembly: System.Reflection.AssemblyProductAttribute("KNNC")]
+[assembly: System.Reflection.AssemblyTitleAttribute("KNNC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
